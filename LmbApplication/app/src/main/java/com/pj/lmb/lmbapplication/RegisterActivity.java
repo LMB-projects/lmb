38f9fd5bb4621d382ebb8b7a19574e0ebcf35fe2
reisato <reika.sato@esm.co.jp>
2016-12-27 18:49:13 +0900

    12.27 commit

--- LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/RegisterActivity.java
+++ LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,6 +13,13 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Button register = (Button)findViewById(R.id.register_button);
+        register.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String message = "登録しました。";
+                Toast toast = Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
 
     }
 }
