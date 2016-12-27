c45bd0863fc983aada8277eb5d4bf428c7adc84b
reisato <reika.sato@esm.co.jp>
2016-12-27 18:04:42 +0900

    commit

--- LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/RegisterActivity.java
+++ LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.pj.lmb.lmbapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button register = (Button)findViewById(R.id.register_button);
+
     }
 }
