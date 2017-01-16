1f8c850cda2e8071205b8e0105572b43555fcd4f
w.yoshizawa <w.yoshizawa@esm.co.jp>
2017-01-16 12:45:15 +0900

    詳細画面マージ・修正

--- LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/DetailsActivity.java
+++ LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/DetailsActivity.java
@@ -1,13 +1,53 @@
 package com.pj.lmb.lmbapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    EditText mTitleEdit;
+    EditText mDataEdit;
+    EditText mTimeEdit;
+    EditText mMultilineEdit;
+    Spinner mSpinnerEdit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+    }
+
+
+    public void onDeleteClickButton(View view) {
+
+        // 確認ダイアログの生成
+        AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
+        alertDlg.setTitle("予定を削除します");
+        alertDlg.setMessage("よろしいですか？");
+        alertDlg.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // OK ボタンクリック処理
+                    }
+                });
+        alertDlg.setNegativeButton(
+                "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Cancel ボタンクリック処理
+                    }
+                });
+
+        // 表示
+        alertDlg.create().show();
+
     }
 }
