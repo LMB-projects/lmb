0452ec187c735e467e4b1f00d6d6dfb7f6082302
reisato <reika.sato@esm.co.jp>
2017-01-18 18:42:56 +0900

    登録処理途中コミット

--- LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/RegisterActivity.java
+++ LmbApplication/app/src/main/java/com/pj/lmb/lmbapplication/RegisterActivity.java
@@ -6,17 +6,40 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText mTitleEdit;
+    EditText mDataEdit;
+    EditText mTimeEdit;
+    EditText mMultilineEdit;
+    Spinner mSpinnerEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mTitleEdit = (EditText) findViewById(R.id.name);
+        mDataEdit = (EditText) findViewById(R.id.date);
+        mTimeEdit = (EditText) findViewById(R.id.time);
+        mMultilineEdit = (EditText) findViewById(R.id.contents);
+        mSpinnerEdit = (Spinner) findViewById(R.id.category);
     }
 
    public void onRegisterBtnClick(View view){
+       RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+       Realm realm = Realm.getInstance(realmConfiguration);
+       realm.beginTransaction();
+
         // 確認ダイアログの作成
        AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
        alertDlg.setTitle("スケジュールの登録");
